56a59fc22135251b69bcd6b23e8fc3f793e5359d
JosePabloMonteroC <pepemonterok2002@gmail.com>
2023-05-29 07:40:34 -0600

    File update

---
+++ src/routing/Paths.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Routes, Route, BrowserRouter, Navigate } from "react-router-dom";
+import { Index } from "../components/pages/Index";
+import { Results } from "../components/pages/Results";
+
+export const Paths = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Index />} />
+        <Route path="/index" element={<Index />} />
+        <Route path="/results" element={<Results />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
