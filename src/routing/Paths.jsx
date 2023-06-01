55e20ec30321cf40712fbc05d9a0fdf7e315fd58
JosePabloMonteroC <pepemonterok2002@gmail.com>
2023-06-01 13:18:38 -0600

    Patched

--- src/routing/Paths.jsx
+++ src/routing/Paths.jsx
@@ -7,7 +7,8 @@ export const Paths = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/relationship-time-tracker/" element={<Index />} />
+        {/* <Route path="/relationship-time-tracker/" element={<Index />} /> */}
+        <Route path="/" element={<Index />} />
         <Route path="/index" element={<Index />} />
         <Route path="/results" element={<Results />} />
       </Routes>
